e3362e04265b2a9a841f13def7b6a623e49b3593
Nivethitha A S <67115292+Nive-2001@users.noreply.github.com>
2022-11-19 23:10:19 +0530

    fff

--- Project Development Phase/Sprint 3/Templates/index.js
+++
@@ -1,7 +0,0 @@
-const toastTrigger = document.getElementById("liveToastBtn");
-const toastLiveExample = document.getElementById("liveToast");
-if (toastTrigger) {
-  toastTrigger.addEventListener("click", () => {
-    alert("Success !!")
-  });
-}
--- Project Development Phase/Sprint 4/CSS/Readme.md
+++
@@ -1,2 +0,0 @@
-I have used internal style(CSS) so i have not attached any files and
-I have used ip address for images so i have not attached any files
